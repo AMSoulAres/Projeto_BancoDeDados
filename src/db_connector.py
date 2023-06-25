a478b39886794cf55180ce7e957149aacc6bb08e
Gabriel Amorim Soares e Silva <amorimsoares@gmail.com>
2023-06-25 11:32:04 -0300

    ajustando criação de banco

--- src/db_connector.py
+++ src/db_connector.py
@@ -1,13 +1,23 @@
 import mysql.connector
+import _mysql_connector
+from src.utils.cria_database import criar_banco
+
+try:
+    db = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="avaliacaounb"
+    )
+    
+except Exception:
+    criar_banco()
 
 db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="root",
-    database="avaliacaounb"
-)
+        host="localhost",
+        user="root",
+        password="root",
+        database="avaliacaounb"
+    )
 
 mycursor = db.cursor()
-
-
-
--- src/utils/cria_database.py
+++ src/utils/cria_database.py
@@ -1,6 +1,13 @@
-from src.db_connector import mycursor
 import mysql.connector.errors as exceptionMySQL
+import mysql.connector
 
+db = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root"
+    )
+
+cursorCriaDB = db.cursor()
 
 def criar_banco():
     openFile = open("./BDSQL.sql", "r")
@@ -8,12 +15,21 @@ def criar_banco():
     openFile.close()
 
     sqlCommands = sqlFile.split(";")
-    mycursor.execute("CREATE DATABASE AvaliacaoUNB")
+    cursorCriaDB.execute("CREATE DATABASE IF NOT EXISTS AvaliacaoUNB")
+
+    dbCriado = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="avaliacaounb"
+    )
+
+    cursorDBCriado = dbCriado.cursor()
 
     for command in sqlCommands:
         try:
             if not command.strip().startswith("--"):
                 print(command)
-                # mycursor.execute(command)
+                cursorDBCriado.execute(command)
         except exceptionMySQL.OperationalError as err:
             print(err)
